3fb0af4c5b84f44e4a8f6ceb6984415b1daa90b7
Sacimia <vinicius.nimia123@gmail.com>
2022-10-04 16:23:57 -0300

    an error maybe at import files

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,26 +1,124 @@
-import "./Tetris.css";
+import React, { useState } from 'react';
 
-import Board from "/src/components/Board";
-import GameStats from "/src/components/GameStats";
-import Previews from "/src/components/Previews";
+import { createStage, checkCollision } from '../gameHelpers';
+import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 
-import { useBoard } from "/src/hooks/useBoard";
-import { useGameStats } from "/src/hooks/useGameStats";
-import { usePlayer} from "/src/hooks/usePlayer";
+// Custom Hooks
+import { useInterval } from '../hooks/useInterval';
+import { usePlayer } from '../hooks/usePlayer';
+import { useStage } from '../hooks/useStage';
+import { useGameStatus } from '../hooks/useGameStatus';
 
-const Tetris = ({ rows, columns, setGameOver }) =>{
-  const [gameStats, addLinesCleared] = useGameStats();
-  const [board, setBoard] = useBoard({
-     rows,
-     columns
-   });
-   const [player, setPlayer ,resetPlayer] = usePlayer();
+// Components
+import Stage from './Stage';
+import Display from './Display';
+import StartButton from './StartButton';
+
+const Tetris = () => {
+  const [dropTime, setDropTime] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+
+  const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
+  const [stage, setStage, rowsCleared] = useStage(player, resetPlayer);
+  const [score, setScore, rows, setRows, level, setLevel] = useGameStatus(
+    rowsCleared
+  );
+
+  console.log('re-render');
+
+  const movePlayer = dir => {
+    if (!checkCollision(player, stage, { x: dir, y: 0 })) {
+      updatePlayerPos({ x: dir, y: 0 });
+    }
+  };
+
+  const keyUp = ({ keyCode }) => {
+    if (!gameOver) {
+      // Activate the interval again when user releases down arrow.
+      if (keyCode === 40) {
+        setDropTime(1000 / (level + 1));
+      }
+    }
+  };
+
+  const startGame = () => {
+    // Reset everything
+    setStage(createStage());
+    setDropTime(1000);
+    resetPlayer();
+    setScore(0);
+    setLevel(0);
+    setRows(0);
+    setGameOver(false);
+  };
+
+  const drop = () => {
+    // Increase level when player has cleared 10 rows
+    if (rows > (level + 1) * 10) {
+      setLevel(prev => prev + 1);
+      // Also increase speed
+      setDropTime(1000 / (level + 1) + 200);
+    }
+
+    if (!checkCollision(player, stage, { x: 0, y: 1 })) {
+      updatePlayerPos({ x: 0, y: 1, collided: false });
+    } else {
+      // Game over
+      if (player.pos.y < 1) {
+        console.log('GAME OVER!!!');
+        setGameOver(true);
+        setDropTime(null);
+      }
+      updatePlayerPos({ x: 0, y: 0, collided: true });
+    }
+  };
+
+  const dropPlayer = () => {
+    setDropTime(null);
+    drop();
+  };
+
+  useInterval(() => {
+    drop();
+  }, dropTime);
+
+  const move = ({ keyCode }) => {
+    if (!gameOver) {
+      if (keyCode === 37) {
+        movePlayer(-1);
+      } else if (keyCode === 39) {
+        movePlayer(1);
+      } else if (keyCode === 40) {
+        dropPlayer();
+      } else if (keyCode === 38) {
+        playerRotate(stage, 1);
+      }
+    }
+  };
+
   return (
-    <div className="Tetris">
-      <Board board={board} />
-      <GameStats gameStats = {gameStats} />
-      <Previous tetrominoes={player.tetrominoes}>
-    </div>
+    <StyledTetrisWrapper
+      role="button"
+      tabIndex="0"
+      onKeyDown={e => move(e)}
+      onKeyUp={keyUp}
+    >
+      <StyledTetris>
+        <Stage stage={stage} />
+        <aside>
+          {gameOver ? (
+            <Display gameOver={gameOver} text="Game Over" />
+          ) : (
+            <div>
+              <Display text={`Score: ${score}`} />
+              <Display text={`rows: ${rows}`} />
+              <Display text={`Level: ${level}`} />
+            </div>
+          )}
+          <StartButton callback={startGame} />
+        </aside>
+      </StyledTetris>
+    </StyledTetrisWrapper>
   );
 };
 
